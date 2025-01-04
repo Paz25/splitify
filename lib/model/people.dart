9e430a1c3e931bca77da5c9288b0c0b0a5ac85a4
Paz25 <pasharakha.255@gmail.com>
2025-01-04 10:28:31 +0700

    "Added People class and allPeople list in lib/model/people.dart"

---
+++ lib/model/people.dart
@@ -0,0 +1,12 @@
+class People {
+  final String name;
+
+  People({required this.name});
+}
+
+final allPeople = [
+  People(name: 'Pasha'),
+  People(name: 'Kaisar'),
+  People(name: 'Gery'),
+  People(name: 'Claudia'),
+];
